88f50786a4beecd0a0ddfc5f51a4be003a35c991
Brent Dixon <brent@thehabdash.com>
2023-04-08 03:23:53 +0000

    script.js

--- script.js
+++ script.js
@@ -3,25 +3,32 @@ const adviceContainer = document.getElementById("advice");
 const adviceContent = document.getElementById("advice-content");
 
 form.addEventListener("submit", async (event) => {
-  event.preventDefault();
+  try {
+    event.preventDefault();
 
-  const income = document.getElementById("income").value;
-  const expenses = document.getElementById("expenses").value;
-  const savings = document.getElementById("savings").value;
-  const debt = document.getElementById("debt").value;
-  const financialGoal = document.getElementById("financial_goal").value;
+    console.log("Form submitted");
 
-  const prompt = `As a highly knowledgeable and experienced financial counselor, provide comprehensive and personalized financial advice for a user with the following financial situation and goals:
-  - Monthly Income: ${income}
-  - Monthly Expenses: ${expenses}
-  - Current Savings: ${savings}
-  - Current Debt: ${debt}
-  - Financial Goal: ${financialGoal}
-  Consider budgeting, savings, debt management, investments, retirement planning, and risk management in your response. Ensure the advice given is clear, actionable, and tailored to the user's unique needs and objectives. Keep in mind current financial trends, market conditions, and best practices in personal finance.`;
+    const income = document.getElementById("income").value;
+    const expenses = document.getElementById("expenses").value;
+    const savings = document.getElementById("savings").value;
+    const debt = document.getElementById("debt").value;
+    const financialGoal = document.getElementById("financial_goal").value;
 
-  const advice = await getFinancialAdvice(prompt);
-  adviceContent.innerText = advice;
-  adviceContainer.style.display = "block";
+    const prompt = `As a highly knowledgeable and experienced financial counselor, provide comprehensive and personalized financial advice for a user with the following financial situation and goals:
+    - Monthly Income: ${income}
+    - Monthly Expenses: ${expenses}
+    - Current Savings: ${savings}
+    - Current Debt: ${debt}
+    - Financial Goal: ${financialGoal}
+    Consider budgeting, savings, debt management, investments, retirement planning, and risk management in your response. Ensure the advice given is clear, actionable, and tailored to the user's unique needs and objectives. Keep in mind current financial trends, market conditions, and best practices in personal finance.`;
+
+    const advice = await getFinancialAdvice(prompt);
+    console.log("Advice received:", advice);
+    adviceContent.innerText = advice;
+    adviceContainer.style.display = "block";
+  } catch (error) {
+    console.error("Error:", error);
+  }
 });
 
 async function getFinancialAdvice(prompt) {
@@ -39,5 +46,6 @@ async function getFinancialAdvice(prompt) {
   });
 
   const data = await response.json();
+  console.log("Advice received:", data.choices[0].text.trim());
   return data.choices[0].text.trim();
 }
