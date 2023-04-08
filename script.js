d2762ca18c0925a43f3648f461aec68591c9157d
Brent Dixon <brent@thehabdash.com>
2023-04-08 01:49:58 +0000

    Updated API key and org information

--- script.js
+++ script.js
@@ -2,33 +2,39 @@ const form = document.getElementById("user-data-form");
 const adviceContainer = document.getElementById("advice");
 const adviceContent = document.getElementById("advice-content");
 
-form.addEventListener("submit", async (event) =>
-  event.preventDefault();
+form.addEventListener("submit", async (event) => {
+  try {
+    event.preventDefault();
 
-  const income = document.getElementById("income").value;
-  const expenses = document.getElementById("expenses").value;
-  const savings = document.getElementById("savings").value;
-  const debt = document.getElementById("debt").value;
-  const financialGoal = document.getElementById("financial_goal").value;
+    console.log("Form submitted");
 
-  const prompt = `As a highly knowledgeable and experienced financial counselor, provide comprehensive and personalized financial advice for a user with the following financial situation and goals:
-  - Monthly Income: ${income}
-  - Monthly Expenses: ${expenses}
-  - Current Savings: ${savings}
-  - Current Debt: ${debt}
-  - Financial Goal: ${financialGoal}
-  Consider budgeting, savings, debt management, investments, retirement planning, and risk management in your response. Ensure the advice given is clear, actionable, and tailored to the user's unique needs and objectives. Keep in mind current financial trends, market conditions, and best practices in personal finance.`;
+    const income = document.getElementById("income").value;
+    const expenses = document.getElementById("expenses").value;
+    const savings = document.getElementById("savings").value;
+    const debt = document.getElementById("debt").value;
+    const financialGoal = document.getElementById("financial_goal").value;
 
-  const advice = await getFinancialAdvice(prompt);
-  adviceContent.innerText = advice;
-  adviceContainer.style.display = "block";
+    const prompt = `As a highly knowledgeable and experienced financial counselor, provide comprehensive and personalized financial advice for a user with the following financial situation and goals:
+    - Monthly Income: ${income}
+    - Monthly Expenses: ${expenses}
+    - Current Savings: ${savings}
+    - Current Debt: ${debt}
+    - Financial Goal: ${financialGoal}
+    Consider budgeting, savings, debt management, investments, retirement planning, and risk management in your response. Ensure the advice given is clear, actionable, and tailored to the user's unique needs and objectives. Keep in mind current financial trends, market conditions, and best practices in personal finance.`;
+
+    const advice = await getFinancialAdvice(prompt);
+    console.log("Advice received:", advice);
+    adviceContent.innerText = advice;
+    adviceContainer.style.display = "block";
+  } catch (error) {
+    console.error("Error:", error);
+  }
 });
 
 async function getFinancialAdvice(prompt) {
   // Replace with your actual GPT-4 API key and endpoint
-  const apiKey = "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";
-  const apiUrl = "https://api.openai.com/v1/chat/completions
-";
+  const apiKey = "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";
+  const apiUrl = "https://api.openai.com/v1/chat/completions?organization=org-g89WT42eik6s4L7X6msERoYW&model=gpt-4";
 
   const response = await fetch(apiUrl, {
     method: "POST",
@@ -40,5 +46,6 @@ async function getFinancialAdvice(prompt) {
   });
 
   const data = await response.json();
+  console.log("Advice received:", data.choices[0].text.trim());
   return data.choices[0].text.trim();
 }
